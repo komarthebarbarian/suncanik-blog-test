ec74a4beb6a6ed9f9d001a16d04b34d4ee66ec60
komarthebarbarian <ajdukmare@gmail.com>
2024-01-11 16:28:04 +0100

    Fifth commit

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,91 +1,129 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { submitComment } from "../services";
 
 const CommentsForm = ({ slug }) => {
   const [error, setError] = useState(false);
   const [localStorage, setLocalStorage] = useState(null);
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const commentEl = useRef();
-  const nameEl = useRef();
-  const emailEl = useRef();
-  const storeDataEl = useRef();
+  const [formData, setFormData] = useState({
+    name: null,
+    email: null,
+    comment: null,
+    storeData: false,
+  });
 
   useEffect(() => {
-    nameEl.current.value = window.localStorage.getItem("name");
-    emailEl.current.value = window.localStorage.getItem("email");
+    setLocalStorage(window.localStorage);
+    const initalFormData = {
+      name: window.localStorage.getItem("name"),
+      email: window.localStorage.getItem("email"),
+      storeData:
+        window.localStorage.getItem("name") ||
+        window.localStorage.getItem("email"),
+    };
+    setFormData(initalFormData);
   }, []);
 
-  const handleCommentSubmition = () => {
+  const onInputChange = (e) => {
+    const { target } = e;
+    if (target.type === "checkbox") {
+      setFormData((prevState) => ({
+        ...prevState,
+        [target.name]: target.checked,
+      }));
+    } else {
+      setFormData((prevState) => ({
+        ...prevState,
+        [target.name]: target.value,
+      }));
+    }
+  };
+
+  const handlePostSubmission = () => {
     setError(false);
-
-    const { value: comment } = commentEl.current;
-    const { value: name } = nameEl.current;
-    const { value: email } = emailEl.current;
-    const { checked: storeData } = storeDataEl.current;
-
-    if (!comment || !name || !email) {
+    const { name, email, comment, storeData } = formData;
+    if (!name || !email || !comment) {
       setError(true);
       return;
     }
-
-    const commentObj = { name, email, comment, slug };
+    const commentObj = {
+      name,
+      email,
+      comment,
+      slug,
+    };
 
     if (storeData) {
-      window.localStorage.setItem("name", name);
-      window.localStorage.setItem("email", email);
+      localStorage.setItem("name", name);
+      localStorage.setItem("email", email);
     } else {
-      window.localStorage.removeItem("name", name);
-      window.localStorage.removeItem("email", email);
+      localStorage.removeItem("name");
+      localStorage.removeItem("email");
     }
 
     submitComment(commentObj).then((res) => {
-      setShowSuccessMessage(true);
-      setTimeout(() => {
-        setShowSuccessMessage(false);
-      }, 3000);
+      if (res.createComment) {
+        if (!storeData) {
+          formData.name = "";
+          formData.email = "";
+        }
+        formData.comment = "";
+        setFormData((prevState) => ({
+          ...prevState,
+          ...formData,
+        }));
+        setShowSuccessMessage(true);
+        setTimeout(() => {
+          setShowSuccessMessage(false);
+        }, 3000);
+      }
     });
   };
 
   return (
     <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
       <h3 className="text-xl mb-8 font-semibold border-b pb-4">
-        Остави коментар
+        Оставите коментар
       </h3>
       <div className="grid grid-cols-1 gap-4 mb-4">
         <textarea
-          ref={commentEl}
+          value={formData.comment}
+          onChange={onInputChange}
+          className="p-4 outline-none w-full rounded-lg h-40 focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
           name="comment"
           placeholder="Ваш коментар"
-          className="p-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
-        ></textarea>
+        />
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4">
         <input
-          name="name"
-          ref={nameEl}
-          placeholder="Име"
+          type="text"
+          value={formData.name}
+          onChange={onInputChange}
           className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
+          placeholder="Име"
+          name="name"
         />
         <input
-          name="email"
-          ref={emailEl}
-          placeholder="Имејл"
+          type="email"
+          value={formData.email}
+          onChange={onInputChange}
           className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
+          placeholder="Имејл"
+          name="email"
         />
       </div>
       <div className="grid grid-cols-1 gap-4 mb-4">
         <div>
           <input
-            ref={storeDataEl}
+            checked={formData.storeData}
+            onChange={onInputChange}
             type="checkbox"
             id="storeData"
             name="storeData"
             value="true"
           />
-          <label
-            className="text-gray-500 cursor-pointer ml-2"
-            htmlFor="storeData"
-          >
+          <label className="text-gray-500 cursor-pointer" htmlFor="storeData">
+            {" "}
             Запамти моје име и имејл.
           </label>
         </div>
@@ -96,8 +134,8 @@ const CommentsForm = ({ slug }) => {
       <div className="mt-8">
         <button
           type="button"
-          onClick={handleCommentSubmition}
-          className="transition duration-500 ease hover:bg-indigo-900 inline-block bg-pink-600 text-lg rounded-full text-white px-8 py-3 cursor-pointer"
+          onClick={handlePostSubmission}
+          className="transition duration-500 ease hover:bg-indigo-900 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer"
         >
           Објави
         </button>
