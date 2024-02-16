30c113f21d06ffbabab304a77bcc52bb0a93ba68
komarthebarbarian <ajdukmare@gmail.com>
2024-02-16 13:27:41 +0100

    16.2.2024. pt4

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -1,43 +1,13 @@
-import React, { useState, useEffect } from "react";
-import { useRouter } from "next/router";
+import React from "react";
 import { getCategories, getCategoryPost } from "../../services";
 import { PostCard, Categories, Loader } from "../../components";
 
-const CategoryPost = ({ posts }) => {
-  const router = useRouter();
-
-  if (router.isFallback) {
-    return <Loader />;
-  }
-
-  const [categoryName, setCategoryName] = useState("Сунчаник блог");
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const { slug } = router.query;
-        const categories = await getCategories();
-        const category = categories.find((category) => category.slug === slug);
-        setCategoryName(category?.name || "Сунчаник блог");
-      } catch (error) {
-        console.error("Error fetching categories:", error);
-      }
-    };
-
-    fetchData();
-  }, [router.query]);
-
-  if (router.isFallback) {
-    return <Loader />;
-  }
-
+const CategoryPost = ({ posts, categoryName }) => {
   const reversedPosts = posts.slice().reverse();
 
   return (
     <div className="container mx-auto px-10 mb-8">
-      <h2 className="text-2xl text-gray-500 font-semibold mb-8 pb-8 text-center border-b border-gray-400">
-        {categoryName}
-      </h2>
+      <h1>{categoryName}</h1>
 
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="col-span-1 lg:col-span-8">
@@ -57,21 +27,27 @@ const CategoryPost = ({ posts }) => {
 
 export default CategoryPost;
 
-// Fetch data at build time
-export async function getStaticProps({ params }) {
-  const posts = await getCategoryPost(params.slug);
+// Fetch data at server-side
+export async function getServerSideProps({ params }) {
+  try {
+    const posts = await getCategoryPost(params.slug);
+    const categories = await getCategories();
+    const category = categories.find(
+      (category) => category.slug === params.slug
+    );
+    const categoryName = category?.name || "Unknown Category";
 
-  return {
-    props: { posts },
-  };
-}
+    return {
+      props: { posts, categoryName },
+    };
+  } catch (error) {
+    console.error("Error fetching data:", error);
 
-// Specify dynamic routes to pre-render pages based on data.
-// The HTML is generated at build time and will be reused on each request.
-export async function getStaticPaths() {
-  const categories = await getCategories();
-  return {
-    paths: categories.map(({ slug }) => ({ params: { slug } })),
-    fallback: true,
-  };
+    return {
+      props: {
+        posts: [],
+        categoryName: "Unknown Category",
+      },
+    };
+  }
 }
