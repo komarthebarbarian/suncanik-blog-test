ea31f8fda8e29155700cc5aedb06a743c2e2383c
komarthebarbarian <ajdukmare@gmail.com>
2024-02-29 00:05:24 +0100

    29.2.2024. Analytics

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import Header from "./Header";
+import Script from "next/script";
 
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
       {children}
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-E7NY2W59JZ"
+      />
+
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+        `}
+      </Script>
     </>
   );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { Layout } from "../components";
+import Script from "next/script";
 // import "tailwindcss/tailwind.css";
 import "@/styles/globals.scss";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+
+      <Script id="google-analytics-script" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+          page_path: window.location.pathname,
+          });
+    `}
+      </Script>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
